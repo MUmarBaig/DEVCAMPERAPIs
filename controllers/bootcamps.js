4129d58842af7b2292cf1f383e6f49e78f0a13b4
mass <mass@DESKTOP-L9NMLB3>
2020-09-09 14:48:59 +0500

    end of module 4

---
+++ controllers/bootcamps.js
@@ -0,0 +1,92 @@
+const Bootcamp = require('../models/Bootcamp')
+
+
+
+//@des          Get all bootcamps
+//@routes       Get /api/vi/bootcamps
+//@access       Public
+exports.getBootcamps = async (req,res,next) => {
+    try {
+        const bootcamps = await Bootcamp.find()
+
+        res.status(200).json({success:true,data:bootcamps})
+    } catch (error) {
+        res.status(400).json({success:false})
+    }
+    
+}
+
+//@des          Get single bootcamps
+//@routes       Get /api/vi/bootcamps/:id
+//@access       Public
+exports.getBootcamp = async (req,res,next) => {
+    try {
+        const bootcamps = await Bootcamp.findById(req.params.id)
+        if(!bootcamps){
+            return res.status(400).json({success:false})
+        }
+        res.status(200).json({success:true,data:bootcamps})
+    } catch (error) {
+        res.status(400).json({success:false})
+    }
+
+};
+
+//@des          Create new bootcamp
+//@routes       Post /api/vi/bootcamps
+//@access       Private
+exports.createBootcamp = async (req,res,next) => {
+
+    try {
+        const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        data:bootcamp
+    });
+    } catch (error) {
+        res.status(400).json({
+            success:false
+        })
+    }
+    
+}
+
+//@des          Update bootcamp
+//@routes       Put /api/vi/bootcamps/:id
+//@access       Private
+exports.updateBootcamp = async (req,res,next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id,req.body,{
+            new:true,
+            runValidators:true
+        });
+
+        if(!bootcamp){
+            return res.status(400).json({success:false})
+        }
+
+        res.status(200).json({success:true,data:bootcamp})
+    } catch (error) {
+        res.status(400).json({success:false})
+    }    
+    
+}
+
+//@des          detete bootcamp
+//@routes       Delete /api/vi//:id
+//@access       Private
+exports.deleteBootcamp = async (req,res,next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
+
+        if(!bootcamp){
+            return res.status(400).json({success:false})
+        }
+
+        res.status(200).json({success:true,data:{}})
+    } catch (error) {
+        res.status(400).json({success:false})
+    }    
+
+}
--- server.js
+++ server.js
@@ -1,16 +1,57 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors');
+const logger = require('./middleware/logger')
+const connectDB = require('./config/db')
+
+
+
+const morgan = require('morgan')
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+//connect to database
+connectDB()
+
+
+
+// Routes
+
+const bootcamps = require('./routes/bootcamps')
+
+
 const app = express();
+
+// Dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'))
+}
+app.use(logger);
+
+//body parser
+app.use(express.json())
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+
+
 
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
     PORT,
     console.log(
-      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
     )
   );
+
+  //handle unhandled rejetion
+  process.on('unhandledRejection',(err,promise) => {
+    console.log(`Error : ${err.message}`.red.bold)
+
+    server.close(() => {
+      process.exit(1)
+    })
+  })
